7c6eff3d28a16428b9bb775d08e92bd9bf29838c
umraan <umraanahmed@gmail.com>
2019-03-20 18:21:53 -0400

    Initial Commit

---
+++ Two Doors/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Two Doors
+//
+//  Created by Umraan Ahmed on 2019-03-20.
+//  Copyright © 2019 Umraan Ahmed. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
